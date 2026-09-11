215766d83b3c82ae3498cd868f98d0548bc16571
Aehtesham Ahmed <aehtesham2004@gmail.com>
2026-09-11 14:46:47 +0530

    lab work

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -2,14 +2,24 @@ import { createReadStream } from "fs";
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") res.end("<h1>Home Page");
-  else if (req.url === "/product") {
+  if (req.url === "/") {
+    res.write("<h1>Home Page</h1>");
+    res.end(`
+            <a href="/product">Product</a><br><br>
+            <a href="/contact">Contact Us</a>
+        `);
+  } else if (req.url === "/product") {
     const stream = createReadStream("product.html", { encoding: "utf-8" });
+    stream.pipe(res);
+  } else if (req.url === "/contact") {
+    const stream = createReadStream("contact.html", { encoding: "utf-8" });
     stream.pipe(res);
   } else {
     res.statusCode = 404;
-    res.end("Not found");
+    res.end("Not Found");
   }
 });
 
-server.listen(3000, () => console.log("prg6 is running..."));
+server.listen(3000, () => {
+  console.log("prg6 is running...");
+});
