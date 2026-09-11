4d2cfb6f97e357cedc725773a28f1ff2a1b934a0
Aehtesham Ahmed <aehtesham2004@gmail.com>
2026-09-11 15:43:28 +0530

    lab 4

---
+++ lab4/users.js
@@ -0,0 +1,6 @@
+// we use in memory database 
+let users = [
+  { id: 1, name: "Ankit Yadav", mob: "878134xxxx", email: "anlityadav@.com" },
+  { id: 1, name: "Amritansh Pandey", mob: "786567xxxx", email: "anlityadav@.com" },
+]
+
